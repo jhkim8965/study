d3bdbe65f6e80ed5fc28a129bb54e65aaa8c9735
jhkim8965 <jh5440@naver.com>
2022-06-11 02:24:09 +0900

    done

--- nomadCoder/vanilaJS/4_login/app.js
+++ nomadCoder/vanilaJS/4_login/app.js
@@ -2,18 +2,29 @@ const $loginForm = document.querySelector('#login-form');
 const $loginInput = document.querySelector('#login-form input');
 const $greeting = document.querySelector('#greeting');
 
-const CLASSNAME_HIDDEN = 'hidden';
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null) {
+  $loginForm.classList.remove(HIDDEN_CLASSNAME);
+  $loginForm.addEventListener('submit', onLoginFormSubmit);
+} else {
+  printGreetings(savedUserName);
+}
 
 function onLoginFormSubmit(info) {
-
   info.preventDefault();  // prevent browser default feature when the form submitted
 
   const userName = $loginInput.value;
-  localStorage.setItem('username', userName);   // save username in localstorage
+  localStorage.setItem(USERNAME_KEY, userName);   // save username in localstorage
 
-  $loginForm.classList.add(CLASSNAME_HIDDEN);
-  $greeting.innerText = 'Hello ' + userName;
-  $greeting.classList.remove(CLASSNAME_HIDDEN);
+  $loginForm.classList.add(HIDDEN_CLASSNAME);
+  printGreetings(userName);
 }
 
-$loginForm.addEventListener('submit', onLoginFormSubmit);
+function printGreetings(userName) {
+  $greeting.innerText = `Hello ${userName}`;
+  $greeting.classList.remove(HIDDEN_CLASSNAME);
+}

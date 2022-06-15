cde3209580ad9c575d904980009ee878ac7127ee
jhkim8965 <jh5440@naver.com>
2022-06-15 22:25:38 +0900

    task done

---
+++ html_css_js/nomadCoder/vanilaJS/7_ToDoList/js/todo.js
@@ -0,0 +1,69 @@
+const $toDoForm = document.getElementById('todo-form');
+const $toDoInput = $toDoForm.querySelector('input');
+const $toDoList = document.getElementById('todo-list');
+
+const TODOS_KEY = 'todos';
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const $li = event.target.parentElement;
+    $li.remove();
+
+    // my idea
+    // toDos.forEach((todo) => {
+    //     if(todo.id == $li.id) {
+    //         toDos.pop(todo);
+    //     }
+    // });
+    // nico idea
+    toDos = toDos.filter(todo => todo.id !== parseInt($li.id));
+    saveToDos();
+
+    console.log($li);
+}
+
+function paintToDo(newTodo) {
+    const $li = document.createElement('li');
+    $li.id = newTodo.id;
+
+    const $span = document.createElement('span');
+    $span.innerText = newTodo.text;
+    $li.appendChild($span);
+
+    const $button = document.createElement('button');
+    $button.innerText = '❌';
+    $button.addEventListener('click', deleteToDo);
+    $li.appendChild($button);
+    
+    $toDoList.appendChild($li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+
+    const newToDo = $toDoInput.value;
+    $toDoInput.value = '';
+    const newTodoObj = {
+        id: Date.now(),
+        text: newToDo,
+    };
+
+    toDos.push(newTodoObj);
+    
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+$toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    // parsedToDos.forEach((todo) => paintToDo(todo));
+    parsedToDos.forEach(paintToDo);
+}

a9c56e2d1de15b4ccecac4700113fdb81e75249f
jhkim8965 <jh5440@naver.com>
2022-06-18 09:29:03 +0900

    backup

---
+++ html_css_js/nomadCoder/vanilaJS/challenge_final/js/star.js
@@ -0,0 +1,26 @@
+const $img = document.querySelector('.night-city .img');
+const $stars = document.querySelector('.night-city .stars');
+const toMakeStarCnt = 100;
+const maxStarSize = 0.5;
+const maxStarAnimateDelay = 10;
+const starAnimateDuration = 10;
+
+let createdStarCount = 0;
+while(createdStarCount < toMakeStarCnt) {
+    const $star = document.createElement('i');
+    const x = Math.floor(Math.random() * $img.offsetWidth) + $img.style.offsetLeft;
+    const y = Math.floor(Math.random() * $img.offsetHeight);
+    const size = Math.random() * maxStarSize;
+
+    $star.style.left = x + 'px';
+    $star.style.top = y + 'px';;
+    $star.style.width = size + 'vh';
+    $star.style.height = size + 'vh';
+
+    $star.style.animationDelay = Math.random() * maxStarAnimateDelay + 's';
+    $star.style.animationDuration = starAnimateDuration + 's';
+
+    $stars.appendChild($star);
+
+    createdStarCount++;
+}
